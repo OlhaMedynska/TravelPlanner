e6a24bb07282cc436e542a3b2bfdc6d27bb8b152
OlhaMedynska <omedynsk@visa.com>
2025-09-02 18:29:34 +0200

    Create CategoryService.java

---
+++ src/main/java/org/example/travelplanner/service/CategoryService.java
@@ -0,0 +1,38 @@
+package org.example.travelplanner.service;
+
+import org.example.travelplanner.entity.Category;
+import org.example.travelplanner.repository.CategoryRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public Category getCategoryById(Long id) {
+        return categoryRepository.findById(id).orElse(null);
+    }
+
+    public Category createCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public Category updateCategory(Long id, Category categoryDetails) {
+        Category category = categoryRepository.findById(id).orElseThrow();
+        category.setName(categoryDetails.getName());
+        return categoryRepository.save(category);
+    }
+
+    public void deleteCategory(Long id) {
+        categoryRepository.deleteById(id);
+    }
+}
